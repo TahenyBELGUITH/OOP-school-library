b7d520ad47c11c016ed1d836c33f16adb9841ba6
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-09-29 09:52:48 +0100

    Fix linters errors

--- student.rb
+++ student.rb
@@ -1,19 +1,19 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :classroomm
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+  def initialize(age, classroomm, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
+    @classroom = classroomm
   end
 
   def play_hooky
     '¯\(ツ)/¯'
   end
 
-  def classroom=(classroom)
-    @classroom = classroom
+  def classroom=(classroomm)
+    @classroom = classroomm
     classroom.students << self unless classroom.students.include?(self)
   end
 end
